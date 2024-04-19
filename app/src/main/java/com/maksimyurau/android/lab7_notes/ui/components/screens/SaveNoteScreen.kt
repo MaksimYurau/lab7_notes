d829f1215ca7e831f90f5bdf55f0b1be660db2a4
maksimyurau <2004softwaredeveloper@gmail.com>
2024-04-19 15:44:21 +0300

    Updated project. Updated SaveNoteScreen.kt. Updated fun SaveNoteScreen(). Added Scaffold(). Added SaveNoteTopAppBar(). Added SaveNoteContent().

--- app/src/main/java/com/maksimyurau/android/lab7_notes/ui/components/screens/SaveNoteScreen.kt
+++ app/src/main/java/com/maksimyurau/android/lab7_notes/ui/components/screens/SaveNoteScreen.kt
@@ -51,13 +51,27 @@ fun SaveNoteScreen(viewModel: MainViewModel) {
     Scaffold(topBar = {
         val isEditingMode: Boolean = noteEntry.id != NEW_NOTE_ID
         SaveNoteTopAppBar(
-            isEditingMode = isEditingMode, onBackClick = {
+            isEditingMode = isEditingMode,
+            onBackClick = {
                 NotesRouter.navigateTo(Screen.Notes)
             },
-            onSaveNoteClick = { }, onOpenColorPickerClick = { }, onDeleteNoteClick = { }
+            onSaveNoteClick = { // Здесь
+               viewModel.saveNote(noteEntry)
+            },
+            onOpenColorPickerClick = { },
+            onDeleteNoteClick = { // Здесь
+                viewModel.moveNoteToTrash(noteEntry)
+            }
         )
     },
-        content = {}
+        content = {// Здесь
+            SaveNoteContent(
+                note = noteEntry,
+                onNoteChange = { updateNoteEntry ->
+                    viewModel.onNoteEntryChange(updateNoteEntry)
+                }
+            )
+        }
     )
 }
 
